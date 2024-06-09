47f6b4118390abace26530d15ff0558bdb909714
rodrigo0345 <rodrigocralha@gmail.com>
2024-06-09 19:10:57 +0000

    changes

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -4,12 +4,13 @@ require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-o>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    }),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-o>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),   -- Add this line for Ctrl + Space
+  }),
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
@@ -18,6 +19,7 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    -- Disable text source by not including it in the sources
   }, {
     { name = 'buffer' },
   }),
